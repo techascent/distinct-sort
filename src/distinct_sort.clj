838bb589b6fba574a787fe6fa092ac0a4587a104
Chris Nuernberger <chris@techascent.com>
2023-10-29 14:40:07 -0600

    Refactoring and working on parallel dataset loading.

--- src/distinct_sort.clj
+++ src/distinct_sort.clj
@@ -28,31 +28,36 @@
 (def vec-data (make-dataset))
 (def obj-data (hamf/object-array-list vec-data))
 
-(defn load-dataset
-  []
-  (ds/->dataset vec-data))
+;;Loading the dataset this way is about twice as fast as simply passing
+;;in the vector of maps and the key structure is set at the very beginning.
+(defn load-dataset-cwise
+  ([] (load-dataset-cwise vec-data))
+  ([vec-data] (ds/->dataset {:name (lznc/map :name vec-data)
+                             :supply (lznc/map :supply vec-data)})))
 
 
-(defn parallel-load-dataset
+(defn parallel-load-dataset-cwise
   []
   (->> (hamf/pgroups
         (count vec-data)
         (fn [^long sidx ^long eidx]
-          (ds/->dataset (hamf/subvec vec-data sidx eidx))))
+          (load-dataset-cwise (hamf/subvec vec-data sidx eidx))))
        (apply ds/concat)))
 
 
-(def ds (parallel-load-dataset))
+(def ds (parallel-load-dataset-cwise))
 
 ;; distinct-sort> schema
 ;; [{:categorical? true, :name :name, :datatype :string, :n-elems 1000000}
 ;;  {:name :supply, :datatype :int32, :n-elems 1000000}]
 (def schema (mapv meta (vals ds)))
 
+(def target-keys (ds/column-names ds))
+
+(defn key-op [f] (mapv f target-keys))
 
 (defn xforms []
-  (xforms/transjuxt [(comp (map :name) (distinct) (xforms/sort) (xforms/into []))
-                     (comp (map :supply) (distinct) (xforms/sort) (xforms/into []))]
+  (xforms/transjuxt (key-op (fn [k] (comp (map k) (distinct) (xforms/sort) (xforms/into []))))
                     vec-data))
 
 (defn via-reduce-kv []
@@ -63,8 +68,8 @@
 
 
 (defn ds-set-sort []
-  [(-> ds :name set sort vec)
-   (-> ds :supply set sort vec)])
+  (key-op #(-> ds % set sort vec)))
+
 
 (defn hamf-sort
   [data]
@@ -75,8 +80,7 @@
 
 
 (defn hamf-lznc []
-  [(->> vec-data (lznc/map :name) hamf/java-hashset hamf-sort)
-   (->> vec-data (lznc/map :supply) hamf/java-hashset hamf-sort)])
+  (key-op #(->> vec-data (lznc/map %) hamf/java-hashset hamf-sort)))
 
 
 (defn treeset
@@ -86,9 +90,7 @@
      (.addAll vec-data))))
 
 
-(defn via-treeset []
-  [(->> vec-data (lznc/map :name) treeset)
-   (->> vec-data (lznc/map :supply) treeset)])
+(defn via-treeset [] (key-op #(->> vec-data (lznc/map %) treeset)))
 
 
 (defn inorder-dedup
@@ -99,24 +101,20 @@
 (defn via-hamf-sort
   "Out of curiosity, how does a pure sort of the data followed by an in-order deduplication compare?"
   []
-  [(->> vec-data (lznc/map :name) hamf-sort inorder-dedup)
-   (->> vec-data (lznc/map :supply) hamf-sort inorder-dedup)])
+  (key-op #(->> vec-data (lznc/map %) hamf-sort inorder-dedup)))
 
 
 (defn ds-java-hashset-sort []
-  [(->> ds :name hamf/java-hashset hamf-sort)
-   (->> ds :supply hamf/java-hashset hamf-sort)])
+  (key-op #(->> ds % hamf/java-hashset hamf-sort)))
 
-(defn set-add!
-  [^Set s v] (.add s v) s)
+(defn set-add! [^Set s v] (.add s v) s)
 
+(defn set-add-all! [^Set s vs] (.addAll s vs) s)
 
 (defn ds-parallel-hashset-union
   []
-  (let [rf (fn [data] (->> (hamf-rf/preduce hamf/mut-set set-add! hamf/union data)
-                           (hamf-sort)))]
-    [(rf (ds :name))
-     (rf (ds :supply))]))
+  (key-op #(->> (hamf-rf/preduce hamf/mut-set set-add! hamf/union (ds %))
+                (hamf-sort))))
 
 (defn take-left
   "Used as a passthrough merge-fn when both sides are equal."
@@ -125,45 +123,41 @@
 
 (defn ds-parallel-concurrent-hashset
   []
-  (let [rf (fn [data]
-             (let [s (hamf-set/java-concurrent-hashset)]
-               (->> (hamf-rf/preduce (constantly s) set-add! take-left data)
-                    (hamf-sort))))]
-    [(rf (ds :name))
-     (rf (ds :supply))]))
+  (key-op #(let [s (hamf-set/java-concurrent-hashset)]
+             (->> (hamf-rf/preduce (constantly s) set-add! take-left (ds %))
+                  (hamf-sort)))))
 
 
 (defn map-singlepass-concurrent-hashset
   []
-  (let [make-reducer (fn [k]
-                       (let [s (hamf-set/java-concurrent-hashset)]
-                         (hamf-rf/parallel-reducer
-                          (constantly s)
-                          #(set-add! %1 (get %2 k))
-                          take-left
-                          hamf-sort)))]
-    (hamf-rf/preduce-reducers [(make-reducer :name)
-                               (make-reducer :supply)]
-                              vec-data)))
+  (hamf-rf/preduce-reducers (key-op (fn [k]
+                                      (let [s (hamf-set/java-concurrent-hashset)]
+                                        (hamf-rf/parallel-reducer
+                                         (constantly s)
+                                         #(set-add! %1 (get %2 k))
+                                         take-left
+                                         hamf-sort))))
+                            vec-data))
 
 
 
 (defn map-singlepass-hashset-union
   []
-  (let [make-reducer (fn [k]
-                       (hamf-rf/parallel-reducer
-                        hamf/mut-set
-                        #(set-add! %1 (get %2 k))
-                        hamf/union
-                        hamf-sort))]
-    (hamf-rf/preduce-reducers [(make-reducer :name)
-                               (make-reducer :supply)]
-                              vec-data)))
+  (hamf-rf/preduce-reducers (key-op (fn [k]
+                                      (hamf-rf/parallel-reducer
+                                       hamf/mut-set
+                                       #(set-add! %1 (get %2 k))
+                                       hamf/union
+                                       hamf-sort)))
+                            vec-data))
 
 
+;;It turns out it is generally faster to use .addAll methods when they are available.
+;;So to target these we do a parallel reduction over the index space represented as
+;;a sequence of tuples of [start-idx end-idx].
 (defn range-reduce-reducers
-  [reducers n-elems]
-  (let [r (hamf-rf/compose-reducers reducers)
+  [reducer-fn n-elems]
+  (let [r (hamf-rf/compose-reducers (key-op reducer-fn))
         init-fn (hamf-proto/->init-val-fn r)
         rfn (hamf-proto/->rfn r)]
     (->> (hamf/pgroups
@@ -176,45 +170,50 @@
 
 (defn map-range-hashset-union
   []
-  (let [make-reducer (fn [k]
-                       (hamf-rf/parallel-reducer
-                        hamf/mut-set
-                        #(do (.addAll ^Set %1 (lznc/map k (hamf/subvec obj-data (%2 0) (%2 1))))
-                             %1)
-                        hamf/union
-                        hamf-sort))]
-    (range-reduce-reducers [(make-reducer :name)
-                            (make-reducer :supply)]
-                           (count vec-data))))
+  (range-reduce-reducers (fn [k]
+                           (hamf-rf/parallel-reducer
+                            hamf/mut-set
+                            #(set-add-all! %1 (lznc/map k (hamf/subvec vec-data (%2 0) (%2 1))))
+                            hamf/union
+                            hamf-sort))
+                         (count vec-data)))
+
+
+(defn map-range-concurrent-hashset
+  []
+  (range-reduce-reducers (fn [k]
+                           (let [s (hamf-set/java-concurrent-hashset)]
+                             (hamf-rf/parallel-reducer
+                              (constantly s)
+                              #(set-add-all! %1 (lznc/map k (hamf/subvec vec-data (%2 0) (%2 1))))
+                              take-left
+                              hamf-sort)))
+                         (count vec-data)))
 
 
 
 (defn ds-cols-custom-singlepass
   []
-  (let [make-reducer (fn [col]
-                       (let [col (dtype/->buffer col)]
-                         (if (= :int32 (dtype/elemwise-datatype col))
-                           (hamf-rf/parallel-reducer
-                            bitmap/->bitmap
-                            (fn [acc rng]
-                              (let [sidx (long (rng 0))
-                                    eidx (long (rng 1))]
-                                (.add ^RoaringBitmap acc (dtype/->int-array (.subBuffer col sidx eidx)))
-                                acc))
-                            #(do (.or ^RoaringBitmap %1 ^RoaringBitmap %2) %1)
-                            bitmap/->random-access)
-                           (hamf-rf/parallel-reducer
-                            hamf/mut-set
-                            (fn [acc rng]
-                              (let [sidx (long (rng 0))
-                                    eidx (long (rng 1))]
-                                (.addAll ^Set acc (.subBuffer col sidx eidx))
-                                acc))
-                            hamf/union
-                            hamf-sort))))]
-    (range-reduce-reducers [(make-reducer (ds :name))
-                            (make-reducer (ds :supply))]
-                           (ds/row-count ds))))
+  (range-reduce-reducers (fn [k]
+                           (let [col (dtype/->buffer (ds k))]
+                             (if (= :int32 (dtype/elemwise-datatype col))
+                               (hamf-rf/parallel-reducer
+                                bitmap/->bitmap
+                                ;;roaring bitmaps have an accelerated add method for blocks of integers
+                                #(do (.add ^RoaringBitmap %1
+                                           ;;the column has an accelerated pathway for copy-to-int-array.
+                                           (dtype/->int-array (.subBuffer col (%2 0) (%2 1))))
+                                     %1)
+                                #(do (.or ^RoaringBitmap %1 ^RoaringBitmap %2) %1)
+                                ;;Converts bitmap back into an random access container (:uint32)
+                                bitmap/->random-access)
+                               (let [s (hamf-set/java-concurrent-hashset)]
+                                 (hamf-rf/parallel-reducer
+                                  (constantly s)
+                                  #(set-add-all! %1 (.subBuffer col (%2 0) (%2 1)))
+                                  take-left
+                                  hamf-sort)))))
+                         (ds/row-count ds)))
 
 
 
@@ -276,59 +275,48 @@
     (merge {:name nm}
            (benchmark-ms (x)))))
 
+(defn- run-it
+  [x]
+  (x)
+  {:name (keyword (str (:name (meta x))))
+   :mean-ms :ok
+   :variance-ms :ok})
+
 
 (defn run-full-benchmark
-  []
-  (println "running full benchmark ...")
-  (let [profile-fns
-        (lznc/concat
-         [#'xforms
-          #'load-dataset
-          #'parallel-load-dataset
-          #'via-reduce-kv
-          #'ds-set-sort
-          #'hamf-lznc
-          #'via-treeset
-          #'via-hamf-sort
-          #'ds-java-hashset-sort
-          #'ds-parallel-hashset-union
-          #'ds-parallel-concurrent-hashset
-          #'map-singlepass-concurrent-hashset
-          #'map-singlepass-hashset-union
-          #'ds-cols-custom-singlepass]
-         (when @duckdb-conn*
-           [#'load-duckdb-data
-            #'duckdb-prepared]))]
-    (-> (ds/->dataset (mapv bench-it profile-fns))
-        (ds/select-columns [:name :mean-ms :variance-ms])
-        (ds/sort-by-column :mean-ms))))
+  ([] (run-full-benchmark bench-it))
+  ([bench-fn]
+   (println "running full benchmark ...")
+   (let [profile-fns
+         (lznc/concat
+          [#'xforms
+           #'load-dataset-cwise
+           #'parallel-load-dataset-cwise
+           #'via-reduce-kv
+           #'ds-set-sort
+           #'hamf-lznc
+           #'via-treeset
+           #'via-hamf-sort
+           #'ds-java-hashset-sort
+           #'ds-parallel-hashset-union
+           #'ds-parallel-concurrent-hashset
+           #'map-singlepass-concurrent-hashset
+           #'map-singlepass-hashset-union
+           #'map-range-hashset-union
+           #'map-range-concurrent-hashset
+           #'ds-cols-custom-singlepass]
+          (when @duckdb-conn*
+            [#'load-duckdb-data
+             #'duckdb-prepared]))]
+     (-> (ds/->dataset (mapv bench-fn profile-fns))
+         (ds/select-columns [:name :mean-ms :variance-ms])
+         (ds/sort-by-column :mean-ms)))))
+
+
+(comment
+  (run-full-benchmark run-it)
+  )
 
 (defn -main
   [& args]
   (println (run-full-benchmark)))
-
-(comment
-
-
-
-  ;;mac m1 timings
-  ;;xforms - 181ms
-  ;;reduce-kv - 181ms
-  ;;dataset - 100ms
-  ;;hamf - 77ms
-  ;;hamf-ds - 50ms
-  ;;hamf-all-out - 27ms
-  ;;hamf-all-out-concurrent - 25ms
-  ;;duckdb - load times eventually get down to 197ms, actual task duckdb - 13.3ms (!!)
-
-
-  ;;desktop timings
-  ;;xforms - 400ms
-  ;;reduce-kv - 385ms
-  ;;dataset - 157ms
-  ;;hamf - 100ms
-  ;;hamf-ds - 68ms
-  ;;hamf-all-out 47ms
-  ;;hamf-all-out-concurrent - 26ms
-  ;;duckdb - 20ms
-  )
